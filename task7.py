4a2412c4b3e27ded7fb5f0f49947c7f242f00292
turduhodzhaev99 <turduhodzhaev99@gmail.com>
2020-07-17 11:42:39 +0600

    first commit

---
+++ task7.py
@@ -0,0 +1,11 @@
+#Task 2
+"""
+Write a script that takes
+in a string and prints its length.
+HINT:
+Question: What do you mean by length?
+Answer: The character count of a string.
+Ex: 'hello!' # --> 6
+"""
+word = input()
+print(len(word))
